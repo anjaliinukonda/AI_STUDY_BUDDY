281477e17802cb0857ab41d06f9deaf4f192e211
anjaliinukonda <anjali.inukonda@gmail.com>
2026-06-11 14:08:38 +0530

    Update Agent.py

--- Agent.py
+++ Agent.py
@@ -3,14 +3,14 @@ from google import genai
 from dotenv import load_dotenv
 import os
 
-# load_dotenv()
+load_dotenv()
 
-# client = genai.Client(
-#     api_key=os.getenv("AQ.Ab8RN6IkdSLl5ZevqD8ntByCf6--oS3yWBxw6Oz4jXvgsaXPpQ")
-# )
 client = genai.Client(
-    api_key="AQ.Ab8RN6LpcpwAnxoSfE3dtGsId_JIiYrMY5ZpiING0-RsHuVW0g"
+    api_key=os.getenv("GEMINI_AI_KEY")
 )
+# client = genai.Client(
+#     api_key="AQ.Ab8RN6LpcpwAnxoSfE3dtGsId_JIiYrMY5ZpiING0-RsHuVW0g"
+# )
 
 st.title("ConceptBridge AI")
 
